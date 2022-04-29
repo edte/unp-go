460ceb2d6c4cf9392f48611d52a668c8065235aa
张彩嵘 <940481948@qq.com>
2022-04-29 08:14:46 +0800

    feat(*): add chapter 3 labs

--- chapter3/3.3/main.go
+++ chapter3/3.3/main.go
@@ -5,21 +5,31 @@
 // @description:
 package main
 
+import (
+	"bufio"
+	"io"
+	syscall "syscall"
+)
+
 // 实现 read n 字节，write n 字节，read 一行
 // readN,writeN,readLine
 
-func readN() {
-
+// read 和 write 时，直接指定 slice 的长度为 n 即可，或者直接创建一个长度为 n
+// 的数组，这样就自动实现读写 n byte 了
+func readN(fd int, n int) ([]byte, int, error) {
+	data := make([]byte, n)
+	n, err := syscall.Read(fd, data)
+	return data, n, err
 }
 
-func readLine() {
-
+func writeN(fd int, data []byte, n int) (int, error) {
+	return syscall.Write(fd, data)
 }
 
-func writeN() {
-
+func readLine(reader io.Reader) ([]byte, bool, error) {
+	r := bufio.NewReader(reader)
+	return r.ReadLine()
 }
 
 func main() {
-
 }
